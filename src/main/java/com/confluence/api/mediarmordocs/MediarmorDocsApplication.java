1fdd37f8cb7e03d8fa4f8424901f36e9b5e4c2b1
Jeril John Panicker <jeriljon@gmail.com>
2021-07-21 21:41:24 -0400

    Initial commit for mediarmor-docs

---
+++ src/main/java/com/confluence/api/mediarmordocs/MediarmorDocsApplication.java
@@ -0,0 +1,13 @@
+package com.confluence.api.mediarmordocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediarmorDocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediarmorDocsApplication.class, args);
+    }
+
+}
